b98a4ae5a9279d28dd95ca63ce6fb435de782f1c
finteger <104397474+finteger@users.noreply.github.com>
2025-03-19 08:26:46 -0600

    Create nullish_coalescing_operator.dart

---
+++ nullish_coalescing_operator.dart
@@ -0,0 +1,16 @@
+
+/*
+The name variable is nullable (String?), meaning it can either hold a String or be null.
+The ?? operator checks if name is null. If it is, it assigns 'Guest' as the value for displayName. Otherwise, it assigns the value of name to displayName.
+*/
+
+void main() {
+  String? name;
+
+  // Using the null coalescing operator
+  String displayName = name ?? 'Guest';
+
+  print(displayName); // Output: Guest
+}
+
+
